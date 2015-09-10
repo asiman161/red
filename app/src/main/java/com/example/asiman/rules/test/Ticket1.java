3a7f7e350d8ba3046dd9408729416a8ad1e156b9
asiman161 <melnichenko1996@yandex.ru>
2015-09-10 18:04:21 +0300

    add answers

--- app/src/main/java/com/example/asiman/rules/test/Ticket1.java
+++ app/src/main/java/com/example/asiman/rules/test/Ticket1.java
@@ -11,6 +11,7 @@ public class Ticket1 {
     private String question;
     private int image;
     private int numberOfAnswer;
+    private int indexOfrightAnswer;
 
 
     public Ticket1(int index) {
@@ -32,6 +33,11 @@ public class Ticket1 {
         return numberOfAnswer;
     }
 
+    public int getIndexOfrightAnswer(){
+        question(index);
+        return indexOfrightAnswer;
+    }
+
 
     public void question(int index) {
         switch (index) {
@@ -39,95 +45,121 @@ public class Ticket1 {
                 this.question = "вопрос 1 билет 1";
                 this.image = R.drawable.image1t1;
                 this.numberOfAnswer = 3;
+                this.indexOfrightAnswer = 1;
                 break;
             case 2:
                 this.question = "вопрос 2 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 4;
+                this.indexOfrightAnswer = 3;
                 break;
             case 3:
                 this.question = "вопрос 3 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 2;
                 break;
             case 4:
                 this.question = "вопрос 4 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 5:
                 this.question = "вопрос 5 билет 1";
                 this.image = R.drawable.image2t1;
+                this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 6:
                 this.question = "вопрос 6 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 7:
                 this.question = "вопрос 7 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 8:
                 this.question = "вопрос 8 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 9:
                 this.question = "вопрос 9 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 10:
                 this.question = "вопрос 10 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 11:
                 this.question = "вопрос 11 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 12:
                 this.question = "вопрос 12 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 13:
                 this.question = "вопрос 13 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 14:
                 this.question = "вопрос 14 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 15:
                 this.question = "вопрос 15 билет 1";
                 this.image = R.drawable.image2t1;
                 this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 16:
                 this.question = "вопрос 16 билет 1";
                 this.image = R.drawable.image2t1;
+                this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 17:
                 this.question = "вопрос 17 билет 1";
                 this.image = R.drawable.image2t1;
+                this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 18:
                 this.question = "вопрос 18 билет 1";
                 this.image = R.drawable.image2t1;
+                this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 19:
                 this.question = "вопрос 19 билет 1";
                 this.image = R.drawable.image2t1;
+                this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
             case 20:
                 this.question = "вопрос 20 билет 1";
                 this.image = R.drawable.image2t1;
+                this.numberOfAnswer = 5;
+                this.indexOfrightAnswer = 1;
                 break;
         }
     }
